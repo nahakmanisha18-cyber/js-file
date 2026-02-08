6840e0e7454f4e69fd363d9b18179e9d70738d8c
nahak <nahakmanisha18@gmail.com>
2026-02-08 18:57:24 +0530

    Add files via upload

--- project3/program.js
+++ project3/program.js
@@ -1,95 +1,48 @@
-//Program : 1
-let greeting = (msg) =>{ console.log(msg);}
-greeting("Good Mornig");
-///Output : Good Mornig
-
-
-//Program : 2
-function cir(r){
-    return 3.14 * r * r;
-}
-console.log("Area of Circle = ", cir(5));
-//Output : Area of Circle =  78.5
-
-
-//Program : 3
-function tri(l,h){
-    return (l * h / 2);
-}
-console.log("Area of Triangle = ",tri(10,10));
-//Output : Area of Triangle =  50
-
-
-//Program : 4
-let rect = function(l,h){
-    return l * h ;
-}
-console.log("Area of rectangle =",rect(5,5));
-//Output :Area of rectangle = 25
-
-
-//program : 5
-let for1 = function(a,b,c){
-    ans = (b * b * 4 * a * c)/(2*a);
-    return ans ;
-}
-console.log("Formula1 = ",for1(5,3,2));
-//Output : Formula1 = 36
-
-
-//Program : 6
-let for2 = function (a, b) {
-    ans2 = (a * a) + (2 * a * b) + (b * b);
-    return ans2;
-}
-console.log("Formula2 = ", for2(5, 6));
-//Output : Formula2 = 121
-
-
-//Program : 7
-let cel = function(f){
-    let c = (f-32 )/ 1.8;
-    return c;
-}
-console.log("Celsius = ",cel(95));
-//Output : Celsius = 35
-
-
-//Program : 8
-let fah = function(c){
-    let f = (c * 1.8) + 32;
-    return f;
-}
-console.log("Fahrenheit = ",fah(35));
-//Output : Fahrenheit = 95
-
-
-//Program : 9
-let che = function(n){
-    if(n % 2 == 0){
-        console.log(n,"is Even number");
-    }else{
-        console.log(n,"is Odd number");
+let images = [
+     {
+        name: "Movie Banner 1",
+        image: "https://static.vecteezy.com/system/resources/thumbnails/002/236/321/small_2x/movie-trendy-banner-vector.jpg"
+     },
+    {
+        name: "Movie Banner 2",
+        image: "https://live.staticflickr.com/4038/4686112993_efe13b4937_h.jpg"
+    },
+    {
+        name: "Movie Banner 3",
+        image: "https://mediaready.videoready.tv/tatasky/image/fetch/f_auto,fl_lossy,q_auto,w_926,h_462/https://li-img-cdn-lb.lionsgateplay.com/mvp/TRAINTOBUSAN0Y2016MEN/TRAINTOBUSAN0Y2016MEN-lgi-landscape-poster-1920X1080-PSTL.jpg"
+    },
+    {
+        name: "Movie Banner 4",
+        image: "https://webneel.com/wnet/file/images/11-16/8-xmen-movie-poster-design.jpg"
+    },
+    {
+        name: "Movie Banner 5",
+        image: "https://host.trivialbeing.org/up/tdk-apr30-hong-kong-banner.jpg"
     }
+];
+
+let index = 0;
+
+document.getElementById("banner").src = images[index].image;
+
+document.getElementById("title").innerHTML = images[index].name;
+
+function nextSlide() {
+    index++;
+   
+    if (index == images.length) {
+        index = 0;
+    }
+    document.getElementById("banner").src = images[index].image;
+    document.getElementById("title").innerHTML = images[index].name;
 }
-che(2);
-//Output :2 'is Even number'
 
-
-//program : 10
-let swa = function(a,b){
-    a = b + a; // 30
-    b = a - b; //20
-    a = a - b; //10
-    console.log("A = ",a);
-    console.log("B = ",b);
+function prevSlide() {
+    index--;
+    if (index < 0) {
+        index = images.length - 1;
+    }
+    document.getElementById("banner").src = images[index].image;
+    document.getElementById("title").innerHTML = images[index].name;
 }
-swa(10,20);
-//Output : A = 20 , B = 10
-
-
-
-
-
-
 
